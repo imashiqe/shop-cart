a5438df8a377daec0baa67b343a92b4161f97d44
Ashiqur Rahman <67426392+imashiqe@users.noreply.github.com>
2022-08-19 17:01:06 +0600

    Shopping Cart Calculation Added

---
+++ js/case.js
@@ -0,0 +1,57 @@
+function updateCaseNumber(isIncrease) {
+    const caseNumberField = document.getElementById('case-number-field');
+    const caseNumberString = caseNumberField.value;
+    const previousCaseNumber = parseInt(caseNumberString);
+
+    let newCaseNumber;
+
+    if(isIncrease === true){
+        newCaseNumber = previousCaseNumber + 1;
+
+    }
+    else{
+        newCaseNumber = previousCaseNumber - 1;
+    }
+
+    caseNumberField.value = newCaseNumber;
+
+    return newCaseNumber;
+}
+
+
+
+
+
+
+
+// document.getElementById('btn-case-plus').addEventListener('click', function(){
+//    const caseNumberField = document.getElementById('case-number-field');
+//    const caseNumberString  = caseNumberField.value;
+//    const  previousCaseNumber = parseInt(caseNumberString);
+
+//    const newCaseNumber = previousCaseNumber + 1;
+//    caseNumberField.value = newCaseNumber;
+// });
+document.getElementById('btn-case-plus').addEventListener('click', function(){
+  const newCaseNumber = updateCaseNumber(true);
+
+  const caseTotalPrice = newCaseNumber * 59;
+  const caseTotalElement = document.getElementById('case-total');
+  caseTotalElement.innerText = caseTotalPrice;
+});
+
+
+// document.getElementById('btn-case-minus').addEventListener('click', function(){
+//     const caseNumberField = document.getElementById('case-number-field');
+//     const caseNumberString  = caseNumberField.value;
+//     const  previousCaseNumber = parseInt(caseNumberString);
+ 
+//     const newCaseNumber = previousCaseNumber - 1;
+//     caseNumberField.value = newCaseNumber;
+//  });
+
+document.getElementById('btn-case-minus').addEventListener('click', function(){
+     updateCaseNumber(false);
+
+     
+ });
